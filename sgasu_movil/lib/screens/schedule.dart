488ed00f4a5eb30cfe11759b1252ece012d20569
alvaro aguirre <alguirre2@gmail.com>
2024-08-09 10:32:11 -0600

    events of schedule

--- sgasu_movil/lib/screens/schedule.dart
+++ sgasu_movil/lib/screens/schedule.dart
@@ -12,16 +12,47 @@ class Schedule extends StatefulWidget {
 }
  
 class _ScheduleState extends State<Schedule> {
+CalendarFormat _calendarFormat=CalendarFormat.month;
+DateTime _focusedDay= DateTime.now();
+DateTime? _selectedDay;
+
+
   DateTime today = DateTime.now();
+
+//Store the events created 
 Map<DateTime, List<Event>> events ={};
 TextEditingController _eventControler = TextEditingController();
+
+late final ValueNotifier<List<Event>>_selectedEvent;
   
-  void _onDaySelected(DateTime day, DateTime focusedDay){
+
+@override
+  void initState(){
+super.initState();
+_selectedDay=_focusedDay;
+_selectedEvent = ValueNotifier(_getEventsForDay(_selectedDay!));
+  }
+  
+@override
+ void dispose(){
+  super.dispose();
+ }
+
+  void _onDaySelected(DateTime selectedDay, DateTime focusedDay){
+    if(!isSameDay(_selectedDay, selectedDay)){
+
     setState(() {
-      today= day;
+_selectedDay =selectedDay;  
+_focusedDay=focusedDay;
+_selectedEvent.value =_getEventsForDay(selectedDay);
     });
+    }
   }
 
+
+List<Event> _getEventsForDay(DateTime day){
+  return events[day] ?? [];
+}
 
   @override
   Widget build(BuildContext context) {
@@ -53,7 +84,18 @@ floatingActionButton: FloatingActionButton(
           controller: _eventControler,
         ),
         ),
-        actions: [],
+        actions: [
+          ElevatedButton(onPressed: (){
+
+                events.addAll({
+                  _selectedDay! : [Event(_eventControler.text)]
+                  });
+          Navigator.of(context).pop();
+          _selectedEvent.value=_getEventsForDay(_selectedDay!);
+          }, 
+          child:Text("hecho")
+          ),
+        ]
       );
     });
   },
@@ -68,24 +110,70 @@ body: Container(
           children: [
               Text("Dia seleccionado "+ today.toString().split(" ")[0]),
               Container(
-                child: TableCalendar(
-                  locale: "en_US",
-                  rowHeight: 43,
-                  headerStyle: 
-                    const HeaderStyle(formatButtonVisible: false,titleCentered: true),
-                    availableGestures: AvailableGestures.all,
-                    selectedDayPredicate: (day) => isSameDay(day, today),
-                  focusedDay: today,
-                  firstDay:DateTime.utc(2020,10,16) ,
-                  lastDay: DateTime.utc(2025,3,14),
-                  onDaySelected: _onDaySelected,
-              ),
-              ),
-              Text(today.toString().split(" ")[0]),
+                child: 
+TableCalendar(
+  firstDay: DateTime.utc(2010,3,14),
+  lastDay: DateTime.utc(2030,3,14),
+  focusedDay: _focusedDay,
+  selectedDayPredicate: (day) => isSameDay(_selectedDay, day),
+  calendarFormat: _calendarFormat,
+  startingDayOfWeek: StartingDayOfWeek.monday,
+  onDaySelected: _onDaySelected,
+  eventLoader: _getEventsForDay,
+  calendarStyle: CalendarStyle(
+    outsideDaysVisible: false,
+  ),
+  onFormatChanged: (format){
+    if(_calendarFormat !=format){
+      setState(() {
+        _calendarFormat=format;
+      });
+    }
+  },
+  onPageChanged: (focusDay){
+    _focusedDay=focusDay;
+  },
+),
+),
+SizedBox(height: 7,),
+Expanded(
+  child: ValueListenableBuilder<List<Event>>(
+    valueListenable: _selectedEvent,
+   builder: (context,value, _){
+    return ListView.builder(
+      itemCount: value.length,
+      itemBuilder: (context,index){
+      return Container(
+        margin: EdgeInsets.symmetric(horizontal: 12,vertical: 4),
+        decoration: BoxDecoration(
+          border: Border.all(),
+          borderRadius: BorderRadius.circular(12)
+        ),
+        child: ListTile(onTap: () => print(""),title: Text('hola ${value[index]}'),),
+      );
+      
+    });
+   }),
+),
+
+
+
+              //   TableCalendar(
+              //     locale: "en_US",
+              //     rowHeight: 43,
+              //     headerStyle: 
+              //       const HeaderStyle(formatButtonVisible: false,titleCentered: true),
+              //       availableGestures: AvailableGestures.all,
+              //       selectedDayPredicate: (day) => isSameDay(day, today),
+              //     focusedDay: today,
+              //     firstDay:DateTime.utc(2020,10,16) ,
+              //     lastDay: DateTime.utc(2025,3,14),
+              //     onDaySelected: _onDaySelected,
+              // ),
+              Text(_selectedDay.toString().split(" ")[0]),
               SizedBox(height: 20),
               Text("aqui habra un widget que muestre la hora"),
-              
-              SizedBox(height: 200),
+              SizedBox(height: 100),
             MaterialButton(highlightColor: AppTheme.backcolorGreen,
             color:AppTheme.backcolorGreen ,
             height: 50,
